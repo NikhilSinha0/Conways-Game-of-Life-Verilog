277b3f086fbc473ae9a0046db1e8ed68f12b949a
Nikhil Sinha <nikhilsinha@outlook.com>
2017-11-14 19:59:51 -0800

    Flip flop array added

---
+++ Conway_Game_of_Life/ee201_GCD_top.v
@@ -0,0 +1,160 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Author:			Shideh Shahidi, Bilal Zafar, Gandhi Puvvada
+// Create Date:		02/25/08
+// File Name:		ee201_GCD_top.v 
+// Description: 
+//
+//
+// Revision: 		2.2
+// Additional Comments: 
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+
+module Conways_top
+		(MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS, // Disable the three memory chips
+
+        ClkPort,                           // the 100 MHz incoming clock signal
+		
+		BtnL, BtnU, BtnD, BtnR,            // the Left, Up, Down, and the Right buttons BtnL, BtnR,
+		BtnC,                              // the center button (this is our reset in most of our designs)
+		Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0, // 8 switches
+		Ld7, Ld6, Ld5, Ld4, Ld3, Ld2, Ld1, Ld0, // 8 LEDs
+		An3, An2, An1, An0,			       // 4 anodes
+		Ca, Cb, Cc, Cd, Ce, Cf, Cg,        // 7 cathodes
+		Dp                                 // Dot Point Cathode on SSDs
+	  );
+
+	/*  INPUTS */
+	// Clock & Reset I/O
+	input		ClkPort;	
+	// Project Specific Inputs
+	input		BtnL, BtnU, BtnD, BtnR, BtnC;	
+	input		Sw7, Sw6, Sw5, Sw4, Sw3, Sw2, Sw1, Sw0;
+	
+	
+	/*  OUTPUTS */
+	// Control signals on Memory chips 	(to disable them)
+	output 	MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS;
+	// Project Specific Outputs
+	// LEDs
+	output 	Ld0, Ld1, Ld2, Ld3, Ld4, Ld5, Ld6, Ld7;
+	// SSD Outputs
+	output 	Cg, Cf, Ce, Cd, Cc, Cb, Ca, Dp;
+	output 	An0, An1, An2, An3;	
+
+	
+	/*  LOCAL SIGNALS */
+	wire		Reset, ClkPort;
+	wire		board_clk, sys_clk;
+	wire [1:0] 	ssdscan_clk;
+	reg [26:0]	DIV_CLK;
+	
+	wire Start_Ack_Pulse;
+	wire in_AB_Pulse, CEN_Pulse, BtnR_Pulse, BtnU_Pulse;
+	wire q_I, q_Run;
+	wire [7:0] A, B, AB_GCD, i_count;
+	reg [7:0] Ain; 
+	reg [7:0] Bin; 
+	reg A_bar_slash_B;
+	reg [3:0]	SSD;
+	wire [3:0]	SSD3, SSD2, SSD1, SSD0; 
+	reg [7:0]  SSD_CATHODES; 
+	
+//------------	
+// Disable the three memories so that they do not interfere with the rest of the design.
+	assign {MemOE, MemWR, RamCS, FlashCS, QuadSpiFlashCS} = 5'b11111;
+	
+	
+//------------
+// CLOCK DIVISION
+
+	BUFGP BUFGP1 (board_clk, ClkPort); 	
+
+	// BUFGP BUFGP2 (Reset, BtnC); In the case of Spartan 3E (on Nexys-2 board), we were using BUFGP to provide global routing for the reset signal. But Spartan 6 (on Nexys-3) does not allow this.
+	assign Reset = BtnC;
+
+//-------------------	
+	// In this design, we run the core design at full 50MHz clock!
+	assign	sys_clk = board_clk;
+	// assign	sys_clk = DIV_CLK[25];
+
+//------------
+// INPUT: SWITCHES & BUTTONS
+	// BtnL is used as both Start and Acknowledge. 
+	// To make this possible, we need a single clock producing  circuit.
+		
+//------------
+// DESIGN
+	// On two pushes of BtnR, numbers A and B are recorded in Ain and Bin
+    // (registers of the TOP) respectively
+	always @ (posedge sys_clk, posedge Reset)
+	begin
+		
+	end
+//------------
+// OUTPUT: LEDS
+	
+	assign {Ld7, Ld6, Ld5, Ld4} = {q_I, q_Sub, q_Mult, q_Done};
+	assign {Ld3, Ld2, Ld1, Ld0} = {BtnL, BtnU, BtnR, BtnD}; // Reset is driven by BtnC
+	// Here
+	// BtnL = Start/Ack
+	// BtnU = Single-Step
+	// BtnR = in_A_in_B
+	// BtnD = not used here
+	
+//------------
+// SSD (Seven Segment Display)
+	
+	//SSDs show Ain and Bin in initial state, A and B in subtract state, and GCD and i_count in multiply and done states.
+	// ****** TODO  in Part 2 ******
+	// assign y = s ? i1 : i0;  // an example of a 2-to-1 mux coding
+	// assign y = s1 ? (s0 ? i3: i2): (s0 ? i1: i0); // an example of a 4-to-1 mux coding
+	assign SSD3 = Ain[7:4];
+	assign SSD3 = Ain[3:0];
+	assign SSD3 = Bin[7:4];
+	assign SSD3 = Bin[3:0];
+	assign An3	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
+	assign An2	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
+	assign An1	=  !((ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
+	assign An0	=  !((ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
+	
+	
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+	begin : SSD_SCAN_OUT
+		case (ssdscan_clk) 
+				  2'b00: SSD = SSD3;	
+				  2'b01: SSD = SSD2;  	
+				  
+				  2'b10: SSD = SSD1;
+				  2'b11: SSD = SSD0;
+		endcase 
+	end
+	
+	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES};
+
+	always @ (SSD) 
+	begin : HEX_TO_SSD
+		case (SSD) 
+			4'b0000: SSD_CATHODES = 8'b00000011; // 0
+			4'b0001: SSD_CATHODES = 8'b10011111; // 1
+			4'b0010: SSD_CATHODES = 8'b00100101; // 2
+			4'b0011: SSD_CATHODES = 8'b00001101; // 3
+			4'b0100: SSD_CATHODES = 8'b10011001; // 4
+			4'b0101: SSD_CATHODES = 8'b01001001; // 5
+			4'b0110: SSD_CATHODES = 8'b01000001; // 6
+			4'b0111: SSD_CATHODES = 8'b00011111; // 7
+			4'b1000: SSD_CATHODES = 8'b00000001; // 8
+			4'b1001: SSD_CATHODES = 8'b00001001; // 9
+			4'b1010: SSD_CATHODES = 8'b00010001; // A
+			4'b1011: SSD_CATHODES = 8'b11000001; // B
+			4'b1100: SSD_CATHODES = 8'b01100011; // C
+			4'b1101: SSD_CATHODES = 8'b10000101; // D
+			4'b1110: SSD_CATHODES = 8'b01100001; // E
+			4'b1111: SSD_CATHODES = 8'b01110001; // F    
+			default: SSD_CATHODES = 8'bXXXXXXXX; // default is not needed as we covered all cases
+		endcase
+	end	
+	
+endmodule
+
